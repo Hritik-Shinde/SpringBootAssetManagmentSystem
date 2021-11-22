9c36eb35eea03c53f83c4c91ff3d32df7f162248
unknown <hritikshinde13@gmail.com>
2021-11-22 10:06:27 +0530

    first commit

---
+++ src/main/java/com/assetmanagmentsystem/AssetManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.assetmanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assetmanagmentsystem/AssetManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assetmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
